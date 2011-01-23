fe28a7d7bc406d7e87b797a53b4a9dd7c93a71de
Gary Talent <gtalent2@gmail.com>
2011-01-22 19:02:02 -0600

    Refined the install.sh and fixed the executable installation.

--- install.sh
+++ install.sh
@@ -1,12 +1,12 @@
 if [ -n "$1" ]
 then
-	echo Installing as $1
+	echo Installing as $1d...
 	cd Code
 	make link
 	cd ../
 	cp Code/main /bin/$1d
 
-	rm -f /bin/$1
+	rm -f /bin/$1d
 	rm -rf /var/www/$1
 	cp -r ServeDir /var/www/$1
 fi
